25e99ba501264e4abf8d6e2b08dd164d87cbd66d
Martin Vysny <mavi@vaadin.com>
2020-11-16 15:07:28 +0200

    Fixed styling of the DialogHeaderBar

--- src/main/java/org/vaadin/example/DialogHeaderBar.java
+++ src/main/java/org/vaadin/example/DialogHeaderBar.java
@@ -1,13 +1,13 @@
 package org.vaadin.example;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * @author Martin Vysny <mavi@vaadin.com>
@@ -18,21 +18,21 @@ public class DialogHeaderBar extends HorizontalLayout {
     private boolean maximized = false;
     private String prevWidth = null;
     private String prevHeight = null;
-    private final Button maximizeRestore = new Button(VaadinIcon.EXPAND_SQUARE.create());
+    private final HeaderBarButton maximizeRestore = new HeaderBarButton();
 
     public void setCaption(String caption) {
         this.caption.setText(caption);
     }
     public DialogHeaderBar(Dialog dialog) {
         this.dialog = dialog;
-        getStyle().set("color", "white");
+        getElement().getStyle().set("user-select", "none"); // prevent caption selection when dragging the dialog
         addClassName("draggable"); // in order for the Dialog to be draggable by the header bar
         setWidthFull();
         addAndExpand(caption);
-        caption.getStyle().set("font-size", "var(--lumo-font-size-xl)");
+        caption.getStyle().set("font-size", "var(--lumo-font-size-l)");
         caption.getStyle().set("padding", "4px 16px");
 
-        getStyle().set("background", "blue");
+        getStyle().set("border-bottom", "solid 1px var(--lumo-contrast-10pct)");
         getStyle().set("overflow", "hidden");  // close button overflows /facepalm
         add(maximizeRestore);
         maximizeRestore.addClickListener(e -> {
@@ -50,19 +50,35 @@ public class DialogHeaderBar extends HorizontalLayout {
             update();
         });
 
-        final Button close = new Button(VaadinIcon.CLOSE_BIG.create());
-        close.addThemeVariants(ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_SMALL);
+        final HeaderBarButton close = new HeaderBarButton(VaadinIcon.CLOSE_BIG);
         add(close);
         close.addClickListener(e -> dialog.close());
 
         update();
     }
 
+    private static class HeaderBarButton extends Icon {
+        public HeaderBarButton() {
+            setSize("16px");
+            getElement().getStyle().set("margin", "8px");
+            getElement().getStyle().set("cursor", "pointer");
+        }
+
+        public HeaderBarButton(VaadinIcon icon) {
+            this();
+            setIcon(icon);
+        }
+
+        public void setIcon(VaadinIcon icon) {
+            getElement().setAttribute("icon", "vaadin:" + icon.name().toLowerCase(Locale.ENGLISH).replace('_', '-'));
+        }
+    }
+
     private void update() {
         if (maximized) {
-            maximizeRestore.setIcon(VaadinIcon.COMPRESS_SQUARE.create());
+            maximizeRestore.setIcon(VaadinIcon.COMPRESS_SQUARE);
         } else {
-            maximizeRestore.setIcon(VaadinIcon.EXPAND_SQUARE.create());
+            maximizeRestore.setIcon(VaadinIcon.EXPAND_SQUARE);
         }
         dialog.setDraggable(!maximized);
         dialog.setResizable(!maximized);
